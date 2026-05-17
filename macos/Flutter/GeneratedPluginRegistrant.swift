b774ea0382fa9760faafd5bc368ad4232f6bc702
dd <dd>
2026-05-17 17:44:21 +0800

    Refactor dependencies and clean up unused packages
    - Updated `pubspec.yaml` to replace `flutter_widget_from_html` with `flutter_widget_from_html_core`.
    - Adjusted version constraints for `sqflite_sqlcipher`.
    - Removed unused dependencies from `pubspec.lock` including `audio_session`, `chewie`, `cupertino_icons`, `dbus`, `flutter_svg`, `just_audio`, `video_player`, and others.
    - Cleaned up `third-party/chewie` by removing build artifacts and unnecessary files.
    - Added `.gitignore` to `third-party/chewie` to exclude build and tool directories.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import app_links
-import audio_session
 import biometric_storage
 import device_info_plus
 import file_picker
@@ -15,7 +14,6 @@ import flutter_secure_storage_macos
 import flutter_web_auth_2
 import hotkey_manager_macos
 import isar_flutter_libs
-import just_audio
 import local_auth_darwin
 import local_notifier
 import mobile_scanner
@@ -30,15 +28,11 @@ import sqflite
 import sqflite_sqlcipher
 import tray_manager
 import url_launcher_macos
-import video_player_avfoundation
-import wakelock_plus
-import webview_flutter_wkwebview
 import window_manager
 import window_to_front
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   BiometricStorageMacOSPlugin.register(with: registry.registrar(forPlugin: "BiometricStorageMacOSPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
@@ -47,7 +41,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   HotkeyManagerMacosPlugin.register(with: registry.registrar(forPlugin: "HotkeyManagerMacosPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
-  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   LocalNotifierPlugin.register(with: registry.registrar(forPlugin: "LocalNotifierPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
@@ -62,9 +55,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SqfliteSqlCipherPlugin.register(with: registry.registrar(forPlugin: "SqfliteSqlCipherPlugin"))
   TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
-  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
-  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
   WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
